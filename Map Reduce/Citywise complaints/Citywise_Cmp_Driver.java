45b594e4c5aa9b16ccea6c64f48000460e9fd368
rahultimbadiya <timbadiyarahul@gmail.com>
2017-03-06 14:58:49 -0800

    This folder contains citywise total complaints from the dataset.
    This programs counts the total number of complaints each city has from
    the dataset. This is specific to the each city so easy to find the
    complaints from particular city.

---
+++ Map Reduce/Citywise complaints/Citywise_Cmp_Driver.java
@@ -0,0 +1,60 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+
+/* 
+ * MapReduce jobs are typically implemented by using a driver class.
+ * The purpose of a driver class is to set up the configuration for the
+ * MapReduce job and to run the job.
+ * Typical requirements for a driver class include configuring the input
+ * and output data formats, configuring the map and reduce classes,
+ * and specifying intermediate data formats.
+ * 
+ * The following is the code for the driver class:
+ */
+public class Citywise_Cmp_Driver {
+
+  public static void main(String[] args) throws Exception {
+
+    /*
+     * The expected command-line arguments are the paths containing
+     * input and output data. Terminate the job if the number of
+     * command-line arguments is not exactly 2.
+     */
+      if(args.length != 2)
+	  {
+	      System.out.printf("Usage:<Queryname><input dir><output dir>\n");
+	      System.exit(-1);
+	  }
+
+    /*
+     * Instantiate a Job object for your job's configuration.  
+     */
+    Job job = new Job();
+    
+   
+    job.setJarByClass(Citywise_Cmp_Driver.class);
+    
+  
+    job.setJobName("CountCityWiseComplaints");
+
+    
+    FileInputFormat.setInputPaths(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+  
+    job.setMapperClass(Citywise_Cmp_Mapper.class);
+    job.setReducerClass(Citywise_Cmp_Reducer.class);
+
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    boolean success = job.waitForCompletion(true);
+    System.exit(success ? 0 : 1);
+  }
+}
---
+++ Map Reduce/Citywise complaints/Citywise_Cmp_Reducer.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/* 
+ * To define a reduce function for your MapReduce job, subclass 
+ * the Reducer class and override the reduce method.
+ * The class definition requires four parameters: 
+ *   The data type of the input key (which is the output key type 
+ *   from the mapper)
+ *   The data type of the input value (which is the output value 
+ *   type from the mapper)
+ *   The data type of the output key
+ *   The data type of the output value
+ */   
+public class Citywise_Cmp_Reducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+  /*
+   * The reduce method runs once for each key received from
+   * the shuffle and sort phase of the MapReduce framework.
+   * The method receives a key of type Text, a set of values of type
+   * IntWritable, and a Context object.
+   */
+  @Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
+           int counter = 0;
+	   
+	   try{
+	       for (IntWritable value: values)
+		   {
+		       counter +=  value.get();
+		   }
+
+	       context.write(key,new IntWritable(counter));
+
+
+	   }
+	   catch(Exception e)
+	       {
+	       }
+     
+
+		
+	}
+}
